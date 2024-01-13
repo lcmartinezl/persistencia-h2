eaf11a65de3f97edeebc03a1fce96812f6497907
unknown <luiscarlos81@gmail.com>
2024-01-12 20:58:40 -0600

    Codelab: Persistencia en Memoria con H2

---
+++ h2/src/main/java/dev/luismartinez/h2/repository/Country.java
@@ -0,0 +1,33 @@
+package dev.luismartinez.h2.repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "countries")
+@Entity
+public class Country {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
